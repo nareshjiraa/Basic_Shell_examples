a5bb8fa8a5bc76365d0858bb9a2e8648f9897057
nareshjiraa <130171324+nareshjiraa@users.noreply.github.com>
2023-06-25 19:34:29 +0530

    4st person commit

--- Howareyou.sh
+++ Howareyou.sh
@@ -3,7 +3,7 @@
 PERSON1="Siva"
 PERSON2="Kumar"
 
-echo"{$PERSON1: Hi, $PERSON2}"
-echo"{$PERSON2: Hello, $PERSON1}"
-echo"{$PERSON1: Good Mornong $PERSON2}"
-echo"{$PERSON2: Very Good Morning $PERSON1}"
+echo "{$PERSON1: Hi, $PERSON2}"
+echo "{$PERSON2: Hello, $PERSON1}"
+echo "{$PERSON1: Good Mornong $PERSON2}"
+echo "{$PERSON2: Very Good Morning $PERSON1}"
