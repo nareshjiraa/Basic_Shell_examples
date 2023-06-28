ad4435adcfdc10f5310decc77e6e284cd693e7fb
nareshjiraa <130171324+nareshjiraa@users.noreply.github.com>
2023-06-28 10:38:34 +0530

    arguments

--- Howareyou.sh
+++ Howareyou.sh
@@ -3,6 +3,9 @@
 PERSON1=$1
 PERSON2=$2
 
+
+
+
 echo "{$PERSON1: Hi, $PERSON2}"
 echo "{$PERSON2: Hello, $PERSON1}"
 echo "{$PERSON1: Good Mornong $PERSON2}"
---
+++ basic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+name="John"
+age=32
+
+echo "Hello World"
+
+echo "My Name is $name"
